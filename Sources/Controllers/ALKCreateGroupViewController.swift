6123f520317581f050ad122b18b386b36512edb9
Mukesh Thawani <mukesh9039@gmail.com>
2017-10-04 18:26:13 +0530

    AL-1911 fix group profile update crash

--- Sources/Controllers/ALKCreateGroupViewController.swift
+++ Sources/Controllers/ALKCreateGroupViewController.swift
@@ -52,6 +52,8 @@ final class ALKCreateGroupViewController: ALKBaseViewController {
     @IBOutlet fileprivate weak var imgGroupProfile: UIImageView!
     fileprivate var tempSelectedImg:UIImage!
     fileprivate var cropedImage: UIImage?
+
+    fileprivate let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
     
     var viewModel: ALKCreateGroupViewModel?
     
@@ -100,9 +102,14 @@ final class ALKCreateGroupViewController: ALKBaseViewController {
                     return
                 }
                 let downloadManager = ALKDownloadManager()
+                activityIndicator.isHidden = false
+                activityIndicator.startAnimating()
+                btnCreateGroup.isEnabled = false
                 downloadManager.upload(image: image, uploadURL: uploadUrl, completion: {
                     imageUrlData in
-
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.isHidden = true
+                    self.btnCreateGroup.isEnabled = true
                     guard let data = imageUrlData, let imageUrl = String(data: data, encoding: .utf8) else {
                         NSLog("GROUP PROFILE PICTURE UPDATE FAILED")
                         return
@@ -128,6 +135,10 @@ final class ALKCreateGroupViewController: ALKBaseViewController {
         if UIApplication.shared.userInterfaceLayoutDirection == .rightToLeft {
             txtfGroupName.textAlignment = .right
         }
+        activityIndicator.center = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
+        activityIndicator.color = UIColor.lightGray
+        view.addSubview(activityIndicator)
+        activityIndicator.isHidden = true
         txtfGroupName.layer.cornerRadius = 10
         txtfGroupName.layer.borderColor = UIColor.mainRed().cgColor
         txtfGroupName.layer.borderWidth = 1
@@ -189,7 +200,7 @@ final class ALKCreateGroupViewController: ALKBaseViewController {
         if let vc = storyboard.instantiateViewController(withIdentifier: "CustomCameraNavigationController") as? ALKBaseNavigationViewController {
             guard let firstVC = vc.viewControllers.first else {return}
             let cameraView = firstVC as! ALKCustomCameraViewController
-            cameraView.setCustomCamDelegate(camMode: .CropOption, camDelegate: self)
+            cameraView.setCustomCamDelegate(camMode: .NoCropOption, camDelegate: self)
             self.present(vc, animated: false, completion: nil)
         }
     }
