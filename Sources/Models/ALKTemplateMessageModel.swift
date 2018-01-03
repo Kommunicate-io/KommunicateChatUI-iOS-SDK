7638abf6d6e3df456a88f0b9d302c149701b9340
Mukesh Thawani <mukesh9039@gmail.com>
2018-01-03 19:49:52 +0530

    Parse messageToSend and sendMessageOnSelection keys and update comments

--- Sources/Models/ALKTemplateMessageModel.swift
+++ Sources/Models/ALKTemplateMessageModel.swift
@@ -9,24 +9,25 @@ import Foundation
 
 open class ALKTemplateMessageModel: NSObject {
 
-    /// Should be a unique identifier
+    /// Should be a unique identifier.
     open var identifier: String
 
-    /// Text to display
+    /// Text to display.
     open var text: String
 
     /// If not set and `sendMessageOnSelection` is true
-    /// then `text` will be used to send the message
+    /// then the value of `text` will be used to send the message.
     open var messageToSend: String?
 
     /// If true then the template will be shown
-    /// irrespective of the message type of last message
+    /// irrespective of the message type of last message.
     open var showInAllCases: Bool = true
 
     open var onlyShowWhenLastMessageIsText: Bool = false
     open var onlyShowWhenLastMessageIsImage: Bool = false
     open var onlyShowWhenLastMessageIsVideo: Bool = false
 
+    /// If set to false then the message will not be sent.
     open var sendMessageOnSelection: Bool = true
 
     public init(identifier: String, text: String) {
@@ -36,6 +37,8 @@ open class ALKTemplateMessageModel: NSObject {
 }
 
 extension ALKTemplateMessageModel {
+
+    /// Json will be parsed and mapped to the model.
     public convenience init?(json: [String: Any]) {
         guard let identifier = json["identifier"] as? String,
             let text = json["text"] as? String
@@ -43,5 +46,12 @@ extension ALKTemplateMessageModel {
                 return nil
         }
         self.init(identifier: identifier, text: text)
+
+        if let messageToSend = json["messageToSend"] as? String {
+            self.messageToSend = messageToSend
+        }
+        if let sendMessageOnSelection = json["sendMessageOnSelection"] as? Bool {
+            self.sendMessageOnSelection = sendMessageOnSelection
+        }
     }
 }
