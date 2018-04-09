18a7dccfe53c7e26fbf32245d90b2273d74caffe
Mukesh Thawani <mukesh9039@gmail.com>
2018-04-09 19:52:00 +0530

    Add buttons and add padding

--- Sources/Views/ALKGenericCardCell.swift
+++ Sources/Views/ALKGenericCardCell.swift
@@ -52,6 +52,14 @@ open class ALKGenericCardCell: UITableViewCell {
         stackView.distribution = .fill
         return stackView
     }()
+
+    open let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
     
     open let mainBackgroundView: UIView = {
         let view = UIView()
@@ -61,13 +69,22 @@ open class ALKGenericCardCell: UITableViewCell {
 
     public enum Padding {
         enum CoverImageView {
-            static var top = 20.0
+            static var top: CGFloat = 5.0
+            static var left: CGFloat = 5.0
+            static var right: CGFloat = -5.0
+        }
+        enum mainStackView {
+            static var bottom: CGFloat = -10.0
+            static var left: CGFloat = 5.0
+            static var right: CGFloat = -5.0
         }
     }
 
-    private let descriptionLabelHeight: CGFloat = 80.0
-    private let titleLabelStackViewHeight: CGFloat = 50.0
-    private let coverImageViewHeight: CGFloat = 80.0
+    open var descriptionLabelHeight: CGFloat = 80.0
+    open var titleLabelStackViewHeight: CGFloat = 50.0
+    open var coverImageViewHeight: CGFloat = 80.0
+
+    open var actionButtons = [UIButton]()
 
     override open func awakeFromNib() {
         super.awakeFromNib()
@@ -75,6 +92,7 @@ open class ALKGenericCardCell: UITableViewCell {
 
     public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpButtons()
         setUpViews()
     }
 
@@ -83,7 +101,19 @@ open class ALKGenericCardCell: UITableViewCell {
     }
 
     static public func rowHeight() -> CGFloat {
-        return 250
+
+        // Update height based on number of buttons
+        // present and if image is set.
+        return 350
+    }
+
+    private func setUpButtons() {
+        actionButtons = (1...3).map {
+            _ in
+            let button = UIButton()
+            button.setTitle("title", for: .normal)
+            return button
+        }
     }
     
     private func setUpViews() {
@@ -92,26 +122,33 @@ open class ALKGenericCardCell: UITableViewCell {
 
         titleStackView.addArrangedSubview(titleLabel)
         titleStackView.addArrangedSubview(subtitleLabel)
+        actionButtons.forEach {
+            buttonStackView.addArrangedSubview($0)
+            $0.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        }
         mainStackView.addArrangedSubview(titleStackView)
         mainStackView.addArrangedSubview(descriptionLabel)
+        mainStackView.addArrangedSubview(buttonStackView)
 
         view.addViewsForAutolayout(views: [mainBackgroundView, coverImageView, mainStackView])
 
-        coverImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        coverImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        coverImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        coverImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: Padding.CoverImageView.top).isActive = true
+        coverImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Padding.CoverImageView.left).isActive = true
+        coverImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Padding.mainStackView.right).isActive = true
         coverImageView.heightAnchor.constraint(equalToConstant: coverImageViewHeight).isActive = true
 
         titleStackView.heightAnchor.constraint(equalToConstant: titleLabelStackViewHeight).isActive = true
         descriptionLabel.heightAnchor.constraint(equalToConstant: descriptionLabelHeight).isActive = true
-        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        mainStackView.topAnchor.constraint(equalTo: coverImageView.bottomAnchor, constant: 5.0).isActive = true
-        mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10.0).isActive = true
+        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Padding.mainStackView.left).isActive = true
+        mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Padding.mainStackView.right).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: coverImageView.bottomAnchor).isActive = true
+        mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Padding.mainStackView.bottom).isActive = true
 
         mainBackgroundView.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor).isActive = true
         mainBackgroundView.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor).isActive = true
         mainBackgroundView.topAnchor.constraint(equalTo: coverImageView.topAnchor).isActive = true
         mainBackgroundView.bottomAnchor.constraint(equalTo: mainStackView.bottomAnchor).isActive = true
+
+
     }
 }
