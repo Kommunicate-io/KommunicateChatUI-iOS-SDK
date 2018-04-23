d063bbcdd80fdc3f1593104abe8b4cb7335ced31
Mukesh Thawani <mukesh9039@gmail.com>
2018-04-23 11:31:09 +0530

    Add generic list model and tests

---
+++ Sources/Models/ALKGenericList.swift
@@ -0,0 +1,32 @@
+//
+//  ALKGenericList.swift
+//  Applozic
+//
+//  Created by Mukesh Thawani on 19/04/18.
+//
+
+import Foundation
+
+public struct ALKGenericListTemplate: Codable {
+    public let templateId: Int
+    public struct Payload: Codable {
+        public let headerImage: String
+        public let headerText: String
+        public struct Element: Codable {
+            public let title: String
+            public let description: String
+            public let imageUrl: String
+            public let defaultActionType: String
+            public let defaultActionUrl: String
+        }
+        public let elements: [Element]
+        public struct Button: Codable {
+            public let type: String
+            public let title: String
+            public let url: URL?
+            public let id: String?
+        }
+        public let buttons: [Button]
+    }
+    public let payload: Payload
+}
