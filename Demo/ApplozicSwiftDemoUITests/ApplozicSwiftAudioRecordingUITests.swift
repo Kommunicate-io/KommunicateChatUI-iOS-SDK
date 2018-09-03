9806bac02ca157bf9cf24efe55d9071a9ab84410
shivampokhriyal <shivampokhriyal1997@gmail.com>
2018-09-03 22:55:22 +0530

    Fix fastlane failing on bitrise

--- Demo/ApplozicSwiftDemoUITests/ApplozicSwiftAudioRecordingUITests.swift
+++ Demo/ApplozicSwiftDemoUITests/ApplozicSwiftAudioRecordingUITests.swift
@@ -17,6 +17,15 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
+        //This is important to ensure that the notification permission popup is handled when the app launches for the first time.
+        addUIInterruptionMonitor(withDescription: "“ApplozicSwiftDemo” Would Like to Send You Notifications") { (alerts) -> Bool in
+            if(alerts.buttons["Allow"].exists){
+                alerts.buttons["Allow"].tap();
+            }
+            return true;
+        }
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
         
@@ -41,17 +50,17 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         sleep(2) //This is important so that screen can load before testing
         XCTAssertTrue(chatbar.exists)
         XCTAssertTrue(button.exists)
-        // Create group message
-        XCTAssertEqual(app.tables.cells.count, 1)
+        
+        let numberOfCells = app.tables.cells.count
         button.press(forDuration: 2.5)
-        XCTAssertEqual(app.tables.cells.element(boundBy: 1).identifier, "audioCell") // check if message is audio
-        XCTAssertEqual(app.tables.cells.count, 2)
+        XCTAssertEqual(app.tables.cells.element(boundBy: numberOfCells).identifier, "audioCell") // check if message is audio
+        XCTAssertEqual(app.tables.cells.count, numberOfCells + 1)
         button.press(forDuration: 5.5)
-        XCTAssertEqual(app.tables.cells.element(boundBy: 2).identifier, "audioCell") // check if message is audio
-        XCTAssertEqual(app.tables.cells.count, 3)
+        XCTAssertEqual(app.tables.cells.element(boundBy: numberOfCells + 1).identifier, "audioCell") // check if message is audio
+        XCTAssertEqual(app.tables.cells.count, numberOfCells + 2)
         button.press(forDuration: 1.3)
-        XCTAssertEqual(app.tables.cells.element(boundBy: 3).identifier, "audioCell") // check if message is audio
-        XCTAssertEqual(app.tables.cells.count, 4)
+        XCTAssertEqual(app.tables.cells.element(boundBy: numberOfCells + 2).identifier, "audioCell") // check if message is audio
+        XCTAssertEqual(app.tables.cells.count, numberOfCells + 3)
         afterTest_deleteNewlyCreatedGroup(app: app, title: groupTitle)
     }
     
@@ -62,17 +71,17 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         sleep(2) //This is important so that screen can load before testing
         XCTAssertTrue(chatbar.exists)
         XCTAssertTrue(button.exists)
-        //create group message
-        XCTAssertEqual(app.tables.cells.count, 1)
+        
+        let numberOfCells = app.tables.cells.count
         let startPoint = button.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
         var finishPoint = chatbar.coordinate(withNormalizedOffset: CGVector(dx: 0.9, dy: 0))
         startPoint.press(forDuration: 5.3, thenDragTo: finishPoint)
-        XCTAssertEqual(app.tables.cells.element(boundBy: 1).identifier, "audioCell") // check if message is audio
-        XCTAssertEqual(app.tables.cells.count, 2)
+        XCTAssertEqual(app.tables.cells.element(boundBy: numberOfCells).identifier, "audioCell") // check if message is audio
+        XCTAssertEqual(app.tables.cells.count, numberOfCells + 1)
         finishPoint = chatbar.coordinate(withNormalizedOffset: CGVector(dx: 0.8, dy: 0))
         startPoint.press(forDuration: 4.3, thenDragTo: finishPoint)
-        XCTAssertEqual(app.tables.cells.element(boundBy: 2).identifier, "audioCell") // check if message is audio
-        XCTAssertEqual(app.tables.cells.count, 3)
+        XCTAssertEqual(app.tables.cells.element(boundBy: numberOfCells + 1).identifier, "audioCell") // check if message is audio
+        XCTAssertEqual(app.tables.cells.count, numberOfCells + 2)
         afterTest_deleteNewlyCreatedGroup(app: app, title: groupTitle)
     }
     
@@ -81,7 +90,8 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         sleep(2) //Proper time to load screen
         let (app, _, button) = beforeTest_createNewGroup(title: groupTitle)
         sleep(2) //This is important so that screen can load before testing
-        XCTAssertEqual(app.tables.cells.count, 1)
+        
+        let numberOfCells = app.tables.cells.count
         button.press(forDuration: 0.0)
         button.press(forDuration: 0.1)
         button.press(forDuration: 0.2)
@@ -92,7 +102,7 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         button.press(forDuration: 0.7)
         button.press(forDuration: 0.8)
         button.press(forDuration: 0.9)
-        XCTAssertEqual(app.tables.cells.count, 1)
+        XCTAssertEqual(app.tables.cells.count, numberOfCells)
         afterTest_deleteNewlyCreatedGroup(app: app, title: groupTitle)
     }
     
@@ -103,18 +113,19 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         sleep(2) //This is important so that screen can load before testing
         XCTAssertTrue(chatbar.exists)
         XCTAssertTrue(button.exists)
-        //create group message
-        XCTAssertEqual(app.tables.cells.count, 1)
+        
+        let numberOfCells = app.tables.cells.count
         let startPoint = button.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
         var finishPoint = chatbar.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
         startPoint.press(forDuration: 2, thenDragTo: finishPoint)
-        XCTAssertEqual(app.tables.cells.count, 1)
+        XCTAssertEqual(app.tables.cells.count, numberOfCells)
         //This is important otherwise drag will work unexpectedly.
         startPoint.press(forDuration: 1)
         finishPoint = chatbar.coordinate(withNormalizedOffset: CGVector(dx: 0.1, dy: 0))
         startPoint.press(forDuration: 2, thenDragTo: finishPoint)
-        XCTAssertEqual(app.tables.cells.count, 1)
+        XCTAssertEqual(app.tables.cells.count, numberOfCells)
         afterTest_deleteNewlyCreatedGroup(app: app, title: groupTitle)
+        
     }
     
     func testAudioRecordButton_SwipeUpShouldCancelRecording() {
@@ -124,22 +135,16 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         sleep(2) //This is important so that screen can load before testing
         XCTAssertTrue(chatbar.exists)
         XCTAssertTrue(button.exists)
-        //create group message
-        XCTAssertEqual(app.tables.cells.count, 1)
+        
+        let numberOfCells = app.tables.cells.count
         let startPoint = button.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)) // center of element
         let finishPoint = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
         startPoint.press(forDuration: 5, thenDragTo: finishPoint)
-        XCTAssertEqual(app.tables.cells.count, 1)
+        XCTAssertEqual(app.tables.cells.count, numberOfCells)
         afterTest_deleteNewlyCreatedGroup(app: app, title: groupTitle)
-        
     }
     
     private func login() {
-        
-        let alert = XCUIApplication().alerts["“ApplozicSwiftDemo” Would Like to Send You Notifications"].buttons["Allow"]
-        if alert.exists{
-            alert.tap()
-        }
         
         let path = Bundle(for: ApplozicSwiftAudioRecordingUITest.self).url(forResource: "Info", withExtension: "plist")
         let dict = NSDictionary(contentsOf: path!) as? [String: Any]
@@ -152,7 +157,7 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         userIdTextField.typeText(userId as! String)
         let passwordSecureTextField = elementsQuery.secureTextFields["Password"]
         passwordSecureTextField.tap()
-        passwordSecureTextField.typeText(password as! String) 
+        passwordSecureTextField.typeText(password as! String)
         elementsQuery.buttons["Get Started"].tap()
     }
     
@@ -170,8 +175,16 @@ class ApplozicSwiftAudioRecordingUITest: XCTestCase {
         searchField.tap()
         searchField.typeText("user2ToTestAudioRecord")
         
+        if app.tables["SelectParticipantTableView"].cells.count == 0{
+            var emptyString = String()
+            let stringValue = searchField.value as! String
+            for _ in stringValue {
+                emptyString += XCUIKeyboardKey.delete.rawValue
+            }
+            searchField.typeText(emptyString)
+        }
         app.tables["SelectParticipantTableView"].cells.allElementsBoundByIndex.first?.tap()
-        app.tables.staticTexts["user2ToTestAudioRecord"].tap()
+        app.tables["SelectParticipantTableView"].cells.allElementsBoundByIndex.first?.tap()
         app.buttons["InviteButton"].tap()
         
         let chatbar = app.otherElements["chatBar"]
