1492960c1b4f2792e9cf8c40f2b3e2ad1284df97
shivampokhriyal <shivampokhriyal1997@gmail.com>
2019-01-09 17:18:53 +0530

    Add playground for ALKCurvedButton

---
+++ Demo/Playground/CurvedButton.playground/Contents.swift
@@ -0,0 +1,17 @@
+/// Usage of `ALKCurvedButton`
+import ApplozicSwift
+import UIKit
+
+var str = "Hello, playground"
+
+/// Using default font and color
+var button = ALKCurvedButton(title: "Demo Button")
+
+/// Using custom font and color
+let font = UIFont.boldSystemFont(ofSize: 20)
+let color = UIColor.red
+button = ALKCurvedButton(title: "Demo Button 2", font: font, color: color)
+
+/// Restricting button width
+button = ALKCurvedButton(title: "Long text inside button for multiline", maxWidth: 200)
+
