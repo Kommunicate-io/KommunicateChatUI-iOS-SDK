363114024ce96c1637f98ec8bc4d53fd6cf06e81
shivampokhriyal <shivampokhriyal1997@gmail.com>
2019-01-10 13:09:55 +0530

    Add snapshot tests for quickreplies

---
+++ Demo/ApplozicSwiftDemoTests/Tests/ALKCurvedButtonSnapshotTests.swift
@@ -0,0 +1,79 @@
+//
+//  ALKCurvedButtonSnapshotTests.swift
+//  ApplozicSwiftDemoTests
+//
+//  Created by Shivam Pokhriyal on 10/01/19.
+//  Copyright © 2019 Applozic. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+@testable import ApplozicSwift
+
+class ALKCurvedButtonSnapshotTests: QuickSpec {
+
+    override func spec() {
+        describe("ALKCurvedButton") {
+            var button: ALKCurvedButton!
+
+            context("with default settings") {
+                beforeEach {
+                    button = ALKCurvedButton(title: "Demo text")
+                }
+                it("has a valid snapshot") {
+                    expect(button).to(haveValidSnapshot())
+                }
+            }
+
+            context("with different font") {
+                beforeEach {
+                    button = ALKCurvedButton(title: "Demo text", font: UIFont.boldSystemFont(ofSize: 40))
+                }
+                it("has a valid snapshot") {
+                    expect(button).to(haveValidSnapshot())
+                }
+            }
+
+            context("with different color") {
+                beforeEach {
+                    button = ALKCurvedButton(title: "Demo text", color: UIColor.red)
+                }
+                it("has a valid snapshot") {
+                    expect(button).to(haveValidSnapshot())
+                }
+            }
+
+            context("with different width") {
+                beforeEach {
+                    button = ALKCurvedButton(title: "Very long text for button", maxWidth: 200)
+                }
+                it("has a valid snapshot") {
+                    expect(button).to(haveValidSnapshot())
+                }
+            }
+
+            context("with different backgroundColor") {
+                beforeEach {
+                    button = ALKCurvedButton(title: "Demo text")
+                    button.backgroundColor = UIColor.yellow
+                }
+                it("has a valid snapshot") {
+                    expect(button).to(haveValidSnapshot())
+                }
+            }
+
+            context("without border") {
+                beforeEach {
+                    button = ALKCurvedButton(title: "Demo text")
+                    button.layer.borderWidth = 0
+                    button.backgroundColor = UIColor.white
+                }
+                it("has a valid snapshot") {
+                    expect(button).to(haveValidSnapshot())
+                }
+            }
+        }
+    }
+
+}
