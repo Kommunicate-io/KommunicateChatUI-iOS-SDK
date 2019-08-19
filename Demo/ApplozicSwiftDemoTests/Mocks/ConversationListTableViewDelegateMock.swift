37e70a4eff980960f84a7314eeed9028936ff919
Sunil Kumar <sunil@applozic.com>
2019-08-19 19:59:11 +0530

    Implement delegate in tests (#249)

--- Demo/ApplozicSwiftDemoTests/Mocks/ConversationListTableViewDelegateMock.swift
+++ Demo/ApplozicSwiftDemoTests/Mocks/ConversationListTableViewDelegateMock.swift
@@ -7,10 +7,19 @@
 //
 
 import Foundation
+import Applozic
 @testable import ApplozicSwift
 
 class ConversationListTableViewDelegateMock: ALKConversationListTableViewDelegate {
-    
+
+    func muteNotification(conversation: ALMessage, isMuted: Bool) {
+
+    }
+
+    func userBlockNotification(userId: String, isBlocked: Bool) {
+
+    }
+
     func tapped(_ chat: ALKChatViewModelProtocol, at index: Int) {
         
     }
@@ -22,5 +31,4 @@ class ConversationListTableViewDelegateMock: ALKConversationListTableViewDelegat
     func scrolledToBottom() {
         
     }
-    
 }
