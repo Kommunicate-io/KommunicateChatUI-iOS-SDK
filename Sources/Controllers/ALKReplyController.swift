ea40def27643d6e1de582d8d2b45906fd89f524e
Sunilkumarr <sunil@applozic.com>
2019-10-24 14:12:05 +0530

    Set the background color for view controller

--- Sources/Controllers/ALKReplyController.swift
+++ Sources/Controllers/ALKReplyController.swift
@@ -158,8 +158,8 @@ class ALKReplyController: UIViewController, Localizable {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(10, green: 10, blue: 10, alpha: 0.2)
         view.isOpaque = false
-        view.backgroundColor = .clear
         setupViews()
         proccessAndUpdateView()
     }
