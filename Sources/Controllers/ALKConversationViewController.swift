daa129bd7aae8f0e7ba1fa2536d057915a250369
Sunilkumarr <sunil@applozic.com>
2020-02-13 20:21:36 +0530

    Move the loader before guard

--- Sources/Controllers/ALKConversationViewController.swift
+++ Sources/Controllers/ALKConversationViewController.swift
@@ -1660,8 +1660,8 @@ extension ALKConversationViewController: ALKConversationViewModelDelegate {
         navigationItem.titleView = loadingIndicator
         loadingIndicator.startLoading(localizationFileName: configuration.localizedStringFileName)
         viewModel.currentConversationProfile { profile in
-            guard let profile = profile else { return }
             self.loadingIndicator.stopLoading()
+            guard let profile = profile else { return }
             self.navigationBar.updateView(profile: profile)
         }
     }
