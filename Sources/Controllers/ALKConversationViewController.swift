c09a166829d07407444ee7eb7eba859b462c7daa
Kirti S <shilwantkirti@gmail.com>
2021-04-02 12:33:52 +0530

    Fix for date-time format issue

--- Sources/Controllers/ALKConversationViewController.swift
+++ Sources/Controllers/ALKConversationViewController.swift
@@ -1626,7 +1626,8 @@ open class ALKConversationViewController: ALKBaseViewController, Localizable {
                 }
             case .dateTimeLocal:
                 if let formViewModelDateTimeLocalItem = element as? FormViewModelDateTimeLocalItem {
-                    postFormData[formViewModelDateTimeLocalItem.label] = String(timeInMillSecs)
+                    let formattedDate = Date.formatedDate(formateString: Date.Formates.DateTimeForForm.formFormat , timeInMillSecs: timeInMillSecs)
+                    postFormData[formViewModelDateTimeLocalItem.label] = String(formattedDate)
                 }
             default:
                 break
