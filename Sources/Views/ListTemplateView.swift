bffd3627348d234b5d3550266b5bcd28ef892767
Sunilkumarr <sunil@applozic.com>
2021-06-01 19:22:07 +0530

    Access the bound from NSString

--- Sources/Views/ListTemplateView.swift
+++ Sources/Views/ListTemplateView.swift
@@ -329,8 +329,7 @@ class ListTemplateView: UIView {
 extension String {
     func getHeightFor(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
-
+        let boundingBox = (self as NSString).boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(boundingBox.height + 10)
     }
 }
