daa366b4f2fc41ccd4b7e8e4aea0d12366f8baf7
Sathyan E <61688116+Sathyan-Elangovan@users.noreply.github.com>
2021-12-17 12:58:21 +0530

    custom event callback change

--- Sources/CustomEvent/ALKCustomEventCallback.swift
+++ Sources/CustomEvent/ALKCustomEventCallback.swift
@@ -6,5 +6,5 @@
 // Created by ___Sathyan Elangovan___ on 09/12/21
 import Foundation
 public protocol ALKCustomEventCallback: AnyObject {
-    func eventTriggered(eventType: ALKCustomEventMap,data: [String: Any]?)
+    func eventTriggered(eventName: ALKCustomEventMap,data: [String: Any]?)
 }
