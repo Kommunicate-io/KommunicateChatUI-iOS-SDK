1653c90774def6eb95166c74b6e2359c245f6d32
Sathyan E <61688116+Sathyan-Elangovan@users.noreply.github.com>
2021-12-20 11:43:07 +0530

    Added method defnitions

--- Sources/CustomEvent/ALKCustomEventHandler.swift
+++ Sources/CustomEvent/ALKCustomEventHandler.swift
@@ -6,10 +6,16 @@
 // Created by ___Sathyan Elangovan___ on 09/12/21
 import Foundation
 
-class ALKCustomEventHandler {
+public class ALKCustomEventHandler {
     public static let shared = ALKCustomEventHandler()
     weak var delegate: ALKCustomEventCallback?
 
+    /**
+         This method is to publish the triggered event data if that particular event is subscribed
+        - Parameters:
+        - triggeredEvent : event type
+        - data : data of triggered event
+     */
     public func publish(triggeredEvent: CustomEvent, data:[String: Any]?) {
         if subscribedEvents.contains(triggeredEvent){
             delegate?.eventTriggered(eventName: triggeredEvent, data: data)
@@ -28,10 +34,14 @@ class ALKCustomEventHandler {
         case video = "Video"
         case gallery = "Gallery"
         case document = "Document"
-
     }
     var subscribedEvents = [CustomEvent]()
-
+    /**
+         This method is to subscribe the events.
+        - Parameters:
+        - eventsList : list of event
+        - eventDelegate : delegate to send the subscribed event data
+     */
     public  func setSubscribedEvents(eventsList: [CustomEvent], eventDelegate:ALKCustomEventCallback){
         delegate = eventDelegate
         subscribedEvents.removeAll()
