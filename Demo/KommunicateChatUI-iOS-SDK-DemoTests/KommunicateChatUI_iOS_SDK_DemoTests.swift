345bbc574d77ea08ca3747431343a4a67edf74fb
Kirti Shilwant <shilwantkirti@gmail.com>
2021-12-31 01:58:11 +0530

    Add target for demo tests

---
+++ Demo/KommunicateChatUI-iOS-SDK-DemoTests/KommunicateChatUI_iOS_SDK_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  KommunicateChatUI_iOS_SDK_DemoTests.swift
+//  KommunicateChatUI-iOS-SDK-DemoTests
+//
+//  Created by Kirti S on 12/31/21.
+//  Copyright © 2021 Applozic. All rights reserved.
+//
+
+import XCTest
+
+class KommunicateChatUI_iOS_SDK_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
