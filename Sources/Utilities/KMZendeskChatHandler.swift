0c938b63ca2e27b38d66f5e8ac09dcf546aeaacf
Sathyan Elangovan <sathya576@gmail.com>
2022-11-28 15:46:58 +0530

    Added zendesk pod as sub pod of KM chat UI & created separate class to handle zendesk chat

---
+++ Sources/Utilities/KMZendeskChatHandler.swift
@@ -0,0 +1,27 @@
+//
+//  KMZendeskChatHandler.swift
+//  KommunicateChatUI-iOS-SDK
+//
+//  Created by sathyan elangovan on 28/11/22.
+//
+
+import Foundation
+import ChatSDK
+import ChatProvidersSDK 
+
+
+public class KMZendeskChatHandler {
+    
+    static let shared = KMZendeskChatHandler()
+    
+    
+    func initiateZendesk(key: String) {
+        
+    }
+    
+    func sendMessage(message: String) {
+        
+    }
+    
+    
+}
