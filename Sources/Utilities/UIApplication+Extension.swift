5b95e56367e39bcc6c80a69978aca2c3b28f9bb3
Sathyan-KM <sathyan.elangovan@kommunicate.io>
2023-02-10 13:34:30 +0530

    added available notation to the UIApplication extension

--- Sources/Utilities/UIApplication+Extension.swift
+++ Sources/Utilities/UIApplication+Extension.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@available(iOSApplicationExtension, unavailable, message: "UIApplication.shared is unavailable in application extensions")
 public extension UIApplication {
     static var main: UIApplication? { UIApplication.value(forKeyPath: #keyPath(UIApplication.shared)) as? UIApplication }
 }
