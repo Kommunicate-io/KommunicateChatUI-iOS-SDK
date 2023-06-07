4b5a33717b06a5126e063de95e0b631ef703511e
sathyan.elangovan@kommunicate.io <sathyan.elangovan@kommunicate.io>
2023-06-07 08:44:52 +0530

    fixed the boundary issue creating api failures

--- Sources/Utilities/ALKHTTPManager.swift
+++ Sources/Utilities/ALKHTTPManager.swift
@@ -191,24 +191,17 @@ class ALKHTTPManager: NSObject {
                     body.append(String(format: "Content-Type:%@\r\n\r\n", task.contentType ?? "").data(using: .utf8)!)
                     body.append(data)
                     body.append(String(format: "\r\n").data(using: .utf8)!)
-//                    body.append(String(format: "--%@\r\n", boundary).data(using: .utf8)!)
-
                 }
-//                body.append(String(format: "--%@--\r\n", boundary).data(using: .utf8)!)
-                
+               
                 
                 if !ALApplozicSettings.getDefaultOverrideuploadUrl().isEmpty {
                     body.append(String(format: "--%@\r\n", boundary).data(using: .utf8)!)
-                    
                     body.append(String(format: "Content-Disposition: form-data; name=\"%@\";\r\n",  "data").data(using: .utf8)!)
-
-//                    body.append(String(format: "Content-Disposition: form-data; name=\"data\";\n" ).data(using: .utf8)!)
                     body.append(String(format: "Content-Type:%@\r\n\r\n", "application/json").data(using: .utf8)!)
-
-//                    body.append(String(format: "Content-Type: application/json\n\n").data(using: .utf8)!)
                     body.append(String(format: "{\"groupId\": \"%@\"}\r\n",task.groupdId ?? "").data(using: .utf8)!)
-                    body.append(String(format: "--%@--\r\n", boundary).data(using: .utf8)!)
                 }
+                
+                body.append(String(format: "--%@--\r\n", boundary).data(using: .utf8)!)
                 
                 request.httpBody = body
                 request.url = task.url
@@ -319,3 +312,4 @@ extension ALKHTTPManager: URLSessionDataDelegate {
         }
     }
 }
+
