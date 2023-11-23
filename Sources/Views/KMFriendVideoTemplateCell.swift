dc56f44f2866787a1aa5b2fef5ad28e0831997c6
Abhijeet Ranjan <abhijeet.ranjan@kommunicate.io>
2023-11-23 16:38:38 +0530

    added Support for YouTube.

--- Sources/Views/KMFriendVideoTemplateCell.swift
+++ Sources/Views/KMFriendVideoTemplateCell.swift
@@ -13,6 +13,13 @@ import UIKit
 class KMFriendVideoTemplateCell : KMVideoTemplateCell {
     let appSettingsUserDefaults = ALKAppSettingsUserDefaults()
 
+    fileprivate lazy var messageView = MessageView(
+        bubbleStyle: MessageTheme.receivedMessage.bubble,
+        messageStyle: MessageTheme.receivedMessage.message,
+        maxWidth: ViewPadding.maxWidth - (ViewPadding.messageViewPadding.left + ViewPadding.messageViewPadding.right)
+    )
+
+    lazy var messageViewHeight = self.messageView.heightAnchor.constraint(equalToConstant: 0)
     
     private var avatarImageView: UIImageView = {
         let imv = UIImageView()
@@ -51,26 +58,37 @@ class KMFriendVideoTemplateCell : KMVideoTemplateCell {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(avatarTappedAction))
         avatarImageView.addGestureRecognizer(tapGesture)
 
-        contentView.addViewsForAutolayout(views: [avatarImageView, nameLabel])
+        contentView.addViewsForAutolayout(views: [avatarImageView, messageView, nameLabel])
+        contentView.bringSubviewToFront(messageView)
         nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 57).isActive = true
         nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -56).isActive = true
-        nameLabel.bottomAnchor.constraint(equalTo: videoTableview.topAnchor, constant: -6).isActive = true
+        nameLabel.bottomAnchor.constraint(equalTo: messageView.topAnchor, constant: -6).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 16).isActive = true
 
         avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 18).isActive = true
         avatarImageView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: 0).isActive = true
 
         avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 9).isActive = true
-        avatarImageView.trailingAnchor.constraint(equalTo: videoTableview.leadingAnchor, constant: -10).isActive = true
+        avatarImageView.trailingAnchor.constraint(equalTo: messageView.leadingAnchor, constant: -10).isActive = true
 
         avatarImageView.heightAnchor.constraint(equalToConstant: 37).isActive = true
         avatarImageView.widthAnchor.constraint(equalTo: avatarImageView.heightAnchor).isActive = true
+        
+        let leftPadding = ChatCellPadding.ReceivedMessage.Message.left
+        let rightPadding = ChatCellPadding.ReceivedMessage.Message.right
+        messageView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+        messageView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: leftPadding).isActive = true
+        messageView.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -1 * rightPadding).isActive = true
+        messageViewHeight.isActive = true
 
+        let templateLeftPadding = CGFloat(ALKMessageStyle.receivedBubble.widthPadding)
+
+        videoTableview.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: templateLeftPadding).isActive = true
         videoTableview.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -56).isActive = true
         videoTableview.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16).isActive = true
 
-        videoTableview.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 6).isActive = true
+        videoTableview.topAnchor.constraint(equalTo: messageView.bottomAnchor, constant: 6).isActive = true
         videoTableview.widthAnchor.constraint(equalToConstant: width * 0.60).isActive = true
 
         timeLabel.leadingAnchor.constraint(equalTo: bubbleView.trailingAnchor, constant: 2).isActive = true
@@ -82,7 +100,16 @@ class KMFriendVideoTemplateCell : KMVideoTemplateCell {
     override func update(viewModel: ALKMessageViewModel) {
         super.update(viewModel: viewModel)
         nameLabel.text = viewModel.displayName
-
+        let isMessageEmpty = viewModel.isMessageEmpty
+        let model = viewModel.messageDetails()
+        messageViewHeight.constant = isMessageEmpty ? 0 :
+            ReceivedMessageViewSizeCalculator().rowHeight(messageModel: model, maxWidth: ViewPadding.maxWidth, padding: ViewPadding.messageViewPadding)
+        if !isMessageEmpty {
+            messageView.update(model: model)
+        } else if #available(iOS 17, *) {
+            messageView.update(model: model)
+        }
+        messageView.updateHeighOfView(hideView: isMessageEmpty, model: model)
         let placeHolder = UIImage(named: "placeholder", in: Bundle.km, compatibleWith: nil)
         guard let url = viewModel.avatarURL else {
             avatarImageView.image = placeHolder
---
+++ Sources/Views/KMYoutubeVideoCell.swift
@@ -0,0 +1,68 @@
+//
+//  KMYoutubeVideoCell.swift
+//  KommunicateChatUI-iOS-SDK
+//
+//  Created by Abhijeet Ranjan on 22/11/23.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+class KMYoutubeVideoCell: UITableViewCell {
+
+    var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.scrollView.isScrollEnabled = false
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.backgroundColor = .gray
+        return webView
+    }()
+
+    var captionLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .gray
+        lbl.font = UIFont.boldSystemFont(ofSize: 12)
+        return lbl
+    }()
+
+    var viewModel: VideoTemplate?
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.isUserInteractionEnabled = true
+        addConstraints()
+        self.backgroundColor = .clear
+    }
+
+    func addConstraints() {
+        addViewsForAutolayout(views: [webView, captionLabel])
+
+        webView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        webView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.48).isActive = true
+
+        captionLabel.topAnchor.constraint(equalTo: webView.bottomAnchor).isActive = true
+        captionLabel.centerXAnchor.constraint(equalTo: webView.centerXAnchor).isActive = true
+        captionLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+
+    func updateVideModel(model: VideoTemplate) {
+        self.viewModel = model
+        captionLabel.text = model.caption ?? ""
+        loadYouTubeVideo(url: model.url)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func loadYouTubeVideo(url: String) {
+        guard let youtubeURL = URL(string: "\(url)?rel=0&showinfo=0&controls=0&modestbranding=1") else {
+            return }
+        
+        let request = URLRequest(url: youtubeURL)
+        webView.load(request)
+    }
+}
