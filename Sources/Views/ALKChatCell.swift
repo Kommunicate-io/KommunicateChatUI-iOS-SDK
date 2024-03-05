2649d8e52850ae83431d211e5e978187df6417fe
sathyan.elangovan@kommunicate.io <sathyan.elangovan@kommunicate.io>
2024-03-05 18:33:51 +0530

    Fixed assignee image is not getting rendered on conversation, conversation list when it is created with title

--- Sources/Views/ALKChatCell.swift
+++ Sources/Views/ALKChatCell.swift
@@ -276,13 +276,16 @@ public final class ALKChatCell: SwipeTableViewCell, Localizable {
         let placeHolder = placeholderImage(placeholder, viewModel: viewModel)
         
         if let avatarImage = viewModel.avatarImage {
-            if let imgStr = viewModel.avatarGroupImageUrl, let imgURL = URL(string: imgStr) {
+              if let imgStr = viewModel.avatarGroupImageUrl, let imgURL = URL(string: imgStr) {
                 let resource = Kingfisher.ImageResource(downloadURL: imgURL, cacheKey: imgStr)
                 avatarImageView.kf.setImage(with: resource, placeholder: avatarImage)
-            } else {
+              } else if let avatar = viewModel.avatar {
+                let resource = Kingfisher.ImageResource(downloadURL: avatar, cacheKey: avatar.absoluteString)
+                avatarImageView.kf.setImage(with: resource, placeholder: placeHolder)
+              } else {
                 avatarImageView.image = placeHolder
-            }
-        } else if let avatar = viewModel.avatar {
+              }
+            } else if let avatar = viewModel.avatar {
             let resource = Kingfisher.ImageResource(downloadURL: avatar, cacheKey: avatar.absoluteString)
             avatarImageView.kf.setImage(with: resource, placeholder: placeHolder)
         } else {
