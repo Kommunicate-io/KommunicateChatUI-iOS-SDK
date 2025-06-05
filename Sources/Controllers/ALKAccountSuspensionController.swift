a08600383f875cb392b11afc0d5d403ebcaf89ed
Abhijeet Ranjan <abhijeet.ranjan@kommunicate.io>
2025-06-05 15:38:47 +0530

    Added the logic to show different message as per the need in the suspension screen.

--- Sources/Controllers/ALKAccountSuspensionController.swift
+++ Sources/Controllers/ALKAccountSuspensionController.swift
@@ -7,17 +7,73 @@
 
 import UIKit
 
-public class ALKAccountSuspensionController: UIViewController {
+public class ALKAccountSuspensionController: UIViewController, Localizable {
+    public var configuration: ALKConfiguration!
+    static let CurrentActivatedPlan = "KM_CURRENT_ACTIVATED_PLAN"
+
+    public required init(configuration: ALKConfiguration) {
+        self.configuration = configuration
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     /// When the close button is tapped this will be called.
     public var closePressed: (() -> Void)?
 
     override public func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupMessageLabel()
     }
 
     @objc func closeButtonAction(_: UIButton) {
         closePressed?()
+    }
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    private enum UserPlanType: String {
+        case trial, churn, other
+
+        static func from(_ plan: String) -> UserPlanType {
+            let lowercasedPlan = plan.lowercased()
+            if lowercasedPlan.contains("trial") {
+                return .trial
+            } else if lowercasedPlan.contains("churn") {
+                return .churn
+            } else {
+                return .other
+            }
+        }
+    }
+
+    func setupMessageLabel() {
+        let userDefaults = UserDefaults(suiteName: "group.kommunicate.sdk") ?? .standard
+        let currentPlan = userDefaults.string(forKey: ALKAccountSuspensionController.CurrentActivatedPlan) ?? ""
+        let planType = UserPlanType.from(currentPlan)
+
+        let (key, defaultValue): (String, String)
+
+        switch planType {
+        case .trial:
+            key = "TrialUserDisconnectionMessage"
+            defaultValue = SystemMessage.SuspendedScreen.TrialUserDisconnectionMessage
+        case .churn:
+            key = "ChurnedUserDisconnectionMessage"
+            defaultValue = SystemMessage.SuspendedScreen.ChurnedUserDisconnectionMessage
+        case .other:
+            key = "MobileNotSupportedMessage"
+            defaultValue = SystemMessage.SuspendedScreen.MobileNotSupportedMessage
+        }
+
+        messageLabel.text = localizedString(
+            forKey: key,
+            withDefaultValue: defaultValue,
+            fileName: configuration.localizedStringFileName
+        )
     }
 
     private func setupViews() {
